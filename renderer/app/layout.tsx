0f68bae8fe8daab3c41ceb4255f8bd7721ab422f
Tyson Hu <tianzheh@buffalo.edu>
2024-09-05 02:34:31 -0400

    init commit

---
+++ renderer/app/layout.tsx
@@ -0,0 +1,31 @@
+import localFont from "next/font/local";
+import "../styles/globals.css";
+import {Metadata} from "next";
+import React from "react";
+
+const geistSans = localFont({
+  src: "/fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+const geistMono = localFont({
+    src: "/fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "100 900",
+});
+
+export const metadata: Metadata = {
+    title: "Eazyreview Admin",
+    description: "Admin panel for Eazyreview",
+};
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="en">
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                {children}
+            </body>
+        </html>
+    );
+}
